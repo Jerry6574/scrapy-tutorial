ec65a53ffbfea8be1e8d3df950e661efe2b982e4
nchannelmosfet <nchannelmosfet@gmail.com>
2020-02-08 19:37:25 -0700

    added spider

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -5,7 +5,11 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+# Scraped data -> Item containers -> JSON/CSV files
+# Scraped data -> Item containers -> Pipeline -> SQL/Mongo database
+
 
 class TutorialPipeline(object):
     def process_item(self, item, spider):
+        print("Pipeline: " + item['title'][0])
         return item
